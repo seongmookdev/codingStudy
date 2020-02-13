16ec018895595082e1b846bc5ca95de48e111110
Seongmook LIM <seongmook.lim@gmail.com>
2020-02-13 17:36:54 +0900

    Create [SWEA]_9088.cpp

---
+++ [SWEA]_9088.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    int t,n,k;
+    int arr[1000]={0,};
+    int temp[1000]={0,};
+    scanf("%d",&t);
+    for(int tc=1; tc<=t; ++tc){
+
+        scanf("%d %d", &n, &k);
+        for(int i=0; i<n; ++i){
+            scanf("%d",&arr[i]);
+        }
+
+    }   
+
+    return 0;
+}
