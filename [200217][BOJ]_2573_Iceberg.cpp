981bdc3048e9c7ce93bee84e3cf9279f1471c705
Seongmook LIM <seongmook.lim@gmail.com>
2020-02-17 22:28:18 +0900

    Update [200217][BOJ]_2573_Iceberg.cpp

--- [200217][BOJ]_2573_Iceberg.cpp
+++ [200217][BOJ]_2573_Iceberg.cpp
@@ -10,7 +10,7 @@ int visited[301][301];
 int result[101];
 const int dy[] = {+1, 0, -1, 0};
 const int dx[] = {0, +1, 0, -1};
-
+int ans;
 
 void dfs(int y, int x){
 
@@ -23,27 +23,27 @@ void dfs(int y, int x){
     }
 
     for(int dir=0; dir<4; ++dir){
+        printf("dir\n");
         int ny = y + dy[dir];
         int nx = x + dx[dir];
-        int pos_cnt=1;
+        int pos_cnt=0;
         int pos[4]={0,};
 
         if(ny<0 || ny>=n || nx<0 || nx>=m) continue;
 
-        for(int y=0; y<n; ++y){
-            for(int x=0; x<m; ++x){
-                pos[0] = backup[ny-1][nx];
-                pos[1] = backup[ny+1][nx];
-                pos[2] = backup[ny][nx-1];
-                pos[3] = backup[ny][nx+1];
+        pos[0] = backup[ny-1][nx];
+        pos[1] = backup[ny+1][nx];
+        pos[2] = backup[ny][nx-1];
+        pos[3] = backup[ny][nx+1];
+        for(int i=0; i<4; ++i){
+            if(pos[i] == 0) pos_cnt++;
+        }
 
-                for(int i=0; i<4; ++i){
-                    if(pos[i] == 0) pos_cnt++;
-                }
-
+        for(int y=1; y<n-1; ++y){
+            for(int x=1; x<m-1; ++x){
                 if(visited[ny][nx]==0 && backup[ny][nx]!=0){
                     backup[ny][nx] -= pos_cnt;
-                    if(backup[ny][nx] <0) backup[ny][nx] = 0;
+                    if(backup[ny][nx]<=0) backup[ny][nx] = 0;
                     dfs(ny, nx);
 //                    visited[ny][nx]=1;
                 }
@@ -80,8 +80,8 @@ int main(){
     }
     */
 
-    for(int y=0; y<n; ++y){
-        for(int x=0; x<m; ++x){
+    for(int y=1; y<n-1; ++y){
+        for(int x=1; x<m-1; ++x){
             if(visited[y][x]==0 && backup[y][x]!=0){
                 dfs(y,x);
                 cnt++;
