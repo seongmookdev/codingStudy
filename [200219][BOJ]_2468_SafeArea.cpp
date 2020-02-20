c7f59ae7c91821e7f2c140116d0b2aef800ec2ef
Seongmook LIM <seongmook.lim@gmail.com>
2020-02-20 17:57:13 +0900

    Update [200219][BOJ]_2468_SafeArea.cpp

--- [200219][BOJ]_2468_SafeArea.cpp
+++ [200219][BOJ]_2468_SafeArea.cpp
@@ -1,24 +1,24 @@
-//URL: https://www.acmicpc.net/problem/2468
 #include <iostream>
+#include <cstring>
 #include <algorithm>
-#include <cstring> //memset
-
 using namespace std;
+
+int N;
+int map[101][101];
+int backup[101][101];
+bool visited[101][101];
+int MAX;
 
 const int dy[] = {+1, 0, -1, 0};
 const int dx[] = {0, +1, 0, -1};
 
-int area[101][101];
-int temp[101][101];
-bool visited[101][101];
-int N;
-
-void copy(int depth){
+int copy(int depth){
     for(int y=0; y<N; ++y){
         for(int x=0; x<N; ++x){
-            if(area[y][x] > depth) temp[y][x] = area[y][x];
+            if(map[y][x] > depth)
+            backup[y][x] = map[y][x];
         }
-    }   
+    }
 }
 
 void dfs(int y, int x){
@@ -31,47 +31,56 @@ void dfs(int y, int x){
 
         if(ny<0 || ny>=N || nx<0 || nx>=N) continue;
 
-        if(!visited[ny][nx] && temp[ny][nx]) dfs(ny, nx);
+        if(!visited[ny][nx] && backup[ny][nx]) dfs(ny, nx);
     }
 }
 
-
 int main(){
+    scanf("%d",&N);
 
-    scanf("%d", &N);
     for(int y=0; y<N; ++y){
         for(int x=0; x<N; ++x){
-            scanf("%d", &area[y][x]);
+            scanf("%d",&map[y][x]);
         }
     }
 
-    int MAX = 0;
     for(int y=0; y<N; ++y){
         for(int x=0; x<N; ++x){
-            if(MAX<area[y][x]) MAX=area[y][x];
+            if(MAX<map[y][x]){
+                MAX = map[y][x];
+            }
         }
-    }    
+    }
+    int result=1;
 
-    int result = 1;
-
-    for(int depth=1; depth<=MAX; depth++){
+    for(int depth=1; depth<=MAX; ++depth){
         memset(visited, false, sizeof(visited));
-        memset(temp, 0, sizeof(temp));
-        copy(depth);    //area --> temp
-
+        memset(backup, 0, sizeof(backup));
+        copy(depth);
         int cnt=0;
         for(int y=0; y<N; ++y){
             for(int x=0; x<N; ++x){
-                if(!visited[y][x] && temp[y][x]){
+                if(!visited[y][x] && backup[y][x]){
                     dfs(y,x);
                     cnt++;
                 }
+                
             }
         }
-        printf("cnt=%d\n",cnt);
         result = max(result, cnt);
-    }
+    } 
 
+/*
+    printf("\n");
+    for(int y=0; y<N; ++y){
+        for(int x=0; x<N; ++x){
+
+            printf("%d ",map[y][x]);
+        }
+        printf("\n");
+    }
+*/
+    
     printf("%d\n",result);
 
     return 0;
