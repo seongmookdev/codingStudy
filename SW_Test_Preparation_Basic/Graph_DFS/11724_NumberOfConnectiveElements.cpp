5b6446cc3187c2199dd44abca2fed4fc403e7144
Seongmook LIM <seongmook.lim@gmail.com>
2020-03-05 13:31:57 +0900

    Create 11724_NumberOfConnectiveElements.cpp

---
+++ SW_Test_Preparation_Basic/Graph_DFS/11724_NumberOfConnectiveElements.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX = 1000+1;
+vector<int>graph[MAX];
+bool visited[MAX];
+
+int N, M;
+
+void dfs(int node){
+    visited[node] = true;
+
+    for(int i=0; i<graph[node].size(); ++i){
+        int next = graph[node][i];
+        if(!visited[next]) dfs(next);
+    }
+
+}
+
+int main(){
+
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<M; ++i){
+        int u, v;
+        scanf("%d %d", &u, &v);
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    int cnt=0;
+
+    for(int i=1; i<N+1; ++i){
+        if(!visited[i]){
+            dfs(i);
+            cnt++;
+        }
+    }
+
+    printf("%d\n",cnt);
+
+    return 0;
+}
