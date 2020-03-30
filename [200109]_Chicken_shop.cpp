7d6340d32c9ea48bf1631e3147b71ce7faaa6fe1
Seongmook LIM <seongmook.lim@gmail.com>
2020-03-30 09:12:37 +0900

    Update [200109]_Chicken_shop.cpp

--- [200109]_Chicken_shop.cpp
+++ [200109]_Chicken_shop.cpp
@@ -1,66 +1,64 @@
-#include <stdio.h>
-#include <algorithm>
+#include <iostream>
 #include <vector>
+
 using namespace std;
 
-struct POSI {
-	int y, x;
+struct INFO{
+    int y;
+    int x;
 };
 
-int n, m, type, ret;
-vector<POSI> house, shop, pick;
+vector<INFO> home, shop, pick;
 
-void dfs(int pos) {
-	if (pick.size() == m) {
+int n, m, ret;
 
-		int candi = 0;
-		for (int i = 0; i < house.size(); ++i) {
-			int min_dist = 1000000;
+void dfs(int pos){
+    if(pick.size() == m){
+        int candi=0;
+        for(int i=0; i<home.size(); ++i){
+            int min_dist = 1e9;
+            for(int j=0; j<pick.size(); ++j){
+                min_dist = min(min_dist, abs(home[i].y-pick[j].y)+
+                                         abs(home[i].x-pick[j].x));
+            }
+            candi += min_dist;
+        }
 
-			for (int j = 0; j < pick.size(); ++j) {
-				min_dist = min(min_dist,
-					abs(house[i].y - pick[j].y) + abs(house[i].x - pick[j].x));
-			}
-			candi += min_dist;
-		}
+        if(ret>candi) ret = candi;
+        return;
+    }
 
-		if (ret > candi) {
-			ret = candi;
-		}
-
-		return;
-	}
-
-	for (int i = pos; i < shop.size(); ++i) {
-		printf("push_back: %d\n",shop[i]);
+    for(int i=pos; i<shop.size(); ++i){
         pick.push_back(shop[i]);
-		dfs(i + 1);
-		pick.pop_back();
-	}
+        dfs(i+1);
+        pick.pop_back();
+    }
 }
 
+int main(){
 
-int main()
-{
-	POSI target;
-	scanf("%d %d", &n, &m);
-	for (int y = 0; y < n; ++y) {
-		for (int x = 0; x < n; ++x) {
-			scanf("%d", &type);
-			if (type == 1) {
-				target.y = y, target.x = x;
-				house.push_back(target);
-			}
-			if (type == 2) {
-				target.y = y, target.x = x;
-				shop.push_back(target);
-			}
-		}
-	}
+    int type;
+    INFO target;
+    scanf("%d %d", &n, &m);
+    for(int y=0; y<n; ++y){
+        for(int x=0; x<n; ++x){
+            scanf("%d", &type);
+            if(type == 1){
+                target.y = y;
+                target.x = x;
+                home.push_back(target);
+            }
+            else if(type == 2){
+                target.y = y;
+                target.x = x;
+                shop.push_back(target);
+            }
+        }
+    }
 
-	ret = 0x7fffffff;
-	dfs(0);
-	printf("%d\n", ret);
+    ret = 1e9;
+    dfs(0);
+    printf("%d\n", ret);
 
-	return 0;
+    return 0;
 }
